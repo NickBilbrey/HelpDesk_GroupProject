511a6531187f8f56cebd0d7e53546869d908fe93
KARBIDE17 <stevewoods17@gmail.com>
2023-06-05 21:55:52 -0400

    Added some content to the helpdesk html and css

---
+++ HelpDesk_FrontEnd/src/app/bookmark-form/bookmark-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookmarkFormComponent } from './bookmark-form.component';
+
+describe('BookmarkFormComponent', () => {
+  let component: BookmarkFormComponent;
+  let fixture: ComponentFixture<BookmarkFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BookmarkFormComponent]
+    });
+    fixture = TestBed.createComponent(BookmarkFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ HelpDesk_FrontEnd/src/app/bookmark-form/bookmark-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmark-form',
+  templateUrl: './bookmark-form.component.html',
+  styleUrls: ['./bookmark-form.component.css']
+})
+export class BookmarkFormComponent {
+
+}
